2c36ffab465b5ad800253bbcdfab876d92565561
TarsioGomides <tarsiogomideslima@hotmail.com>
2021-04-11 00:29:29 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/br/com/micro/services/PaymentService.java
@@ -0,0 +1,12 @@
+package br.com.micro.services;
+
+import br.com.micro.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days) {
+        return new Payment("Bob", 200.0, days);
+    }
+}
